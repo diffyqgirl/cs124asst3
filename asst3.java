a16f18fa99b2c6f569fe6b5bd1a8cd935c5b04f6
Laura Ungar <ungar@college.harvard.edu>
2017-04-20 13:48:48 -0400

    got the number generator working, took way too long

--- asst3.java
+++ asst3.java
@@ -1,7 +1,15 @@
 public class asst3
 {
+    static final int N = 100;
     public static void main(String[] args)
     {
-        
+        File infile = new File(args[0]);
+        FileReader fr = new FileReader(infile);
+        BufferedReader br = new BufferedReader(fr);
+        long[] nums = new long[N];
+        for (int i = 0; i < N; i++)
+        {
+            nums[i] = Long.parseLong(br.readLine());
+        }
     }
 }
